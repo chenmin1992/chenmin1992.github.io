f2df3707fc0eabe87091e394031a659dce890492
chenmin <new.1234>
2016-07-18 09:28:04 +0800

    auto push

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
 @echo off
-(git add -A)&&(git commit -m 'add_some_posts')&&(git push)&&(echo Publish Complete!)||(echo Error While Publish!)
+(git add -A)&&(git commit -m "auto push")&&(git push)&&(echo Publish Complete!)||(echo Error While Publish!)
 pause
